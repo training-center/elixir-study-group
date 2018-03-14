1a9837f7df86c6564b8910ab855e20dbd4e5350f
Giovanni Naufal <giovanni.naufal@locaweb.com.br>
2018-03-14 09:02:44 -0300

    Simplify desafio_01/gionaufal.ex

--- solucoes/desafio_01/gionaufal.ex
+++ solucoes/desafio_01/gionaufal.ex
@@ -1,14 +1,10 @@
-defmodule WordFinder do
-  def start do
-    IO.puts "Olá! Estou pensando em uma palavra, que tal adivinhar?"
-    IO.puts "Vou te dar uma dica: é o nome dado a uma poção mágica ou líquido encantado em um frasco..."
-    IO.puts "(e, por acaso, o nome dessa linguagem)\n"
-    input = IO.gets "Digite a palavra: "
-    case input |> String.trim |> String.downcase do
-      "elixir" ->
-        IO.puts "\nVocê acertou!"
-      _ ->
-        IO.puts "\nResposta errada :("
-    end
-  end
+IO.puts "Olá! Estou pensando em uma palavra, que tal adivinhar?"
+IO.puts "Vou te dar uma dica: é o nome dado a uma poção mágica ou líquido encantado em um frasco..."
+IO.puts "(e, por acaso, o nome dessa linguagem)\n"
+input = IO.gets "Digite a palavra: "
+case input |> String.trim |> String.downcase do
+  "elixir" ->
+    IO.puts "\nVocê acertou!"
+  _ ->
+    IO.puts "\nResposta errada :("
 end
