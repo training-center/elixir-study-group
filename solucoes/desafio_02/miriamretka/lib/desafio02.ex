6186b56e5f6f34f7866a04567591c144bdf05bc7
Auralcat <ashleyzt@hi2.in>
2018-03-17 20:09:48 -0300

    Refactor mostrar_dica and adivinhar_palavra for generic args

--- solucoes/desafio_02/miriamretka/lib/desafio02.ex
+++ solucoes/desafio_02/miriamretka/lib/desafio02.ex
@@ -1,18 +1,33 @@
 defmodule Desafio02 do
   @moduledoc """
-  Documentation for Desafio02.
-  """
+  Construa o mesmo jogo acima, porém com algumas modificações:
 
-  @doc """
-  Hello world.
+  - Faça a palavra a ser adivinhada ser configurável.
+  - O usuário tem tentativas ilimitadas.
 
-  ## Examples
+  A palavra em si deve ser entrada logo após rodar o programa. Ao
+  errar, o programa deve voltar a perguntar qual a palavra correta
+  novamente, até que ele acerte."""
 
-      iex> Desafio02.hello
-      :world
+  def mostrar_dica(lista_dicas) do
+    "Dica: " <> Random.choice(lista_dicas)
+  end
 
-  """
-  def hello do
-    :world
+  def adivinhar_palavra(palavra_alvo) do
+    tentativa = IO.gets("Digite a palavra >> ") |> String.replace(~r/\r|\n/, "")
+    if (String.downcase(tentativa) == palavra_alvo) do
+      IO.puts("Parabéns, você acertou!")
+    else
+      IO.puts("Errado, você perdeu.")
+    end
+  end
+
+  def main do
+    IO.puts("Olá, prepare-se para adivinhar a palavra!")
+    IO.puts(mostrar_dica(["Lepidóptero", "faz casulo",
+                          "da mesma família das mariposas"]))
+    adivinhar_palavra("borboleta")
   end
 end
+
+Desafio02.main
