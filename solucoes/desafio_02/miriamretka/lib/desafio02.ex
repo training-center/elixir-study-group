34dbe2a945c8e8eff3d642cbeda83ecb7976f531
Auralcat <ashleyzt@hi2.in>
2018-03-17 19:59:37 -0300

    Start challenge 02

---
+++ solucoes/desafio_02/miriamretka/lib/desafio02.ex
@@ -0,0 +1,18 @@
+defmodule Desafio02 do
+  @moduledoc """
+  Documentation for Desafio02.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Desafio02.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ solucoes/desafio_02/miriamretka/test/desafio02_test.exs
@@ -0,0 +1,8 @@
+defmodule Desafio02Test do
+  use ExUnit.Case
+  doctest Desafio02
+
+  test "greets the world" do
+    assert Desafio02.hello() == :world
+  end
+end
