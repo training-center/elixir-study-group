d34832e0bbd4beabdf3900d1e0dcf1304dc8d069
Auralcat <ashleyzt@hi2.in>
2018-03-20 22:33:02 -0300

    Start challenge 01

---
+++ solucoes/desafio_01/miriamretka/desafio01.exs
@@ -0,0 +1,21 @@
+defmodule Desafio01 do
+  @moduledoc
+  """
+  Construa um jogo de texto, em que o jogador tenha que adivinhar a
+  palavra elixir. Ao rodar o jogo, o programa deve mostrar algumas
+  dicas como Poção mágica, liquido encantado em frasco. O jogador só
+  terá 1 tentativa, ou ele ganha o jogo ou perde. Se ele ganhar, uma
+  mensagem de vitória deverá ser mostrada como Parabéns, você
+  acertou!. Se ele perder, uma mensagem de derrota deverá ser mostrada
+  como Errado, você perdeu.
+  """
+
+
+  def main do
+    IO.puts "Prepare-se para adivinhar a palavra!"
+
+
+  end
+end
+
+Desafio01.main()
