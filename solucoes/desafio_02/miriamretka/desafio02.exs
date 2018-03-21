4d109118385549192749c5e86a6ed1d03253d3f1
Auralcat <ashleyzt@hi2.in>
2018-03-20 23:47:01 -0300

    Finish challenge 02

--- solucoes/desafio_02/miriamretka/desafio02.exs
+++ solucoes/desafio_02/miriamretka/desafio02.exs
@@ -1,23 +1,25 @@
 defmodule Desafio02 do
   def mostrarDica(array_dicas) do
     Enum.random(array_dicas)
-  end
+  en
 
   def adivinharPalavra(palavra_alvo) do
     tentativa = IO.gets("Digite a palavra 👉  ")
       |> String.replace(~r/\r|\n/, "")
       |> String.trim
-    if (String.downcase(tentativa) == "elixir") do
+    if (String.downcase(tentativa) == palavra_alvo) do
       IO.puts "Parabéns, você acertou! 🙌"
     else
-      IO.puts "Que pena, melhor sorte na próxima! 😞"
+      IO.puts "Que pena, tente de novo! 😞"
       adivinharPalavra(palavra_alvo)
     end
   end
 
   def main do
     IO.puts "Prepare-se para adivinhar a palavra!"
-    IO.puts "Dica: #{mostrarDica()}"
+    dicas = ["Lepidóptero", "nasce como lagarta",
+             "mesma família das mariposas"]
+    IO.puts "Dica: #{mostrarDica(dicas)}"
     adivinharPalavra("borboleta")
   end
 end
